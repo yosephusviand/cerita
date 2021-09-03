de70ffb16fdafae3de0e9cf8f0d1428927dde61e
Yosephus Viand <yosephzviand@gmail.com>
2021-09-03 18:24:35 +0700

    peringatan

--- resources/views/pages/cernak.blade.php
+++ resources/views/pages/cernak.blade.php
@@ -25,19 +25,27 @@
                 <div class="col-lg-12">
                     <div class="card" style="opacity: 80%">
                         <div class="body">
+                            <div class="alert alert-danger" role="alert">
+                                1. Seluruh naskah di dalam Pojok Migunani dilindungi undang-undang dan dipergunakan hanya
+                                untuk kepentingan Sosialisasi Wawasan Kebangsaan Bagi Anak. <br>
+                                2. Dilarang keras memperbanyak/menjiplak/membajak/menyalahgunakan naskah dalam Pojok
+                                Migunani untuk kepentingan pribadi
+                            </div>
                             <div class="row">
                                 @foreach ($data as $val)
                                     <div class="col-lg-4 col-md-12">
                                         <div class="form-group">
                                             <embed src="{{ route('cernak.pdf', $val->id) }}" type="application/pdf"
                                                 width="100%">
-                                                <label for="">{{ $val->judul }}</label>
+                                            <label for="">{{ $val->judul }}</label>
                                             <div class="row">
                                                 <div class="col-6">
-                                                    <a href="{{ route('cernak.pdfdownload', $val->id) }}" class="btn btn-primary btn-block">Unduh</a>
+                                                    <a href="{{ route('cernak.pdfdownload', $val->id) }}"
+                                                        class="btn btn-primary btn-block">Unduh</a>
                                                 </div>
                                                 <div class="col-6">
-                                                    <a href="{{ route('cernak.pdf', $val->id) }}" class="btn btn-primary btn-block" target="_blank">Baca</a>
+                                                    <a href="{{ route('cernak.pdf', $val->id) }}"
+                                                        class="btn btn-primary btn-block" target="_blank">Baca</a>
                                                 </div>
                                             </div>
                                         </div>
